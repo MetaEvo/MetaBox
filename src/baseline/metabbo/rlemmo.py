11b268d8c865f6c5b4abc8a18abf64797f632644
WenJie Qiu <100810925+Wukong-SCUT@users.noreply.github.com>
2025-07-03 09:04:14 +0800

    Update rlemmo.py

--- src/baseline/metabbo/rlemmo.py
+++ src/baseline/metabbo/rlemmo.py
@@ -459,7 +459,7 @@ class RLEMMO(PPO_Agent):
                     memory.clear_memory()
                     _Rs = _R.detach().numpy().tolist()
                     return_info = {'return': _Rs,'loss': _loss, 'learn_steps': self.learning_time, }
-                    env_cost = env.get_env_attr('cost')
+                    env_cost = np.array(env.get_env_attr('cost'))
                     return_info['gbest'] = env_cost[:,-1]
                     for key in required_info.keys():
                         return_info[key] = env.get_env_attr(required_info[key])
@@ -471,7 +471,7 @@ class RLEMMO(PPO_Agent):
         is_train_ended = self.learning_time >= self.config.max_learning_step
         _Rs = _R.detach().numpy().tolist()
         return_info = {'return': _Rs,'loss': _loss, 'learn_steps': self.learning_time,}
-        env_cost = env.get_env_attr('cost')
+        env_cost = np.array(env.get_env_attr('cost'))
         return_info['gbest'] = env_cost[:,-1]
         for key in required_info.keys():
             return_info[key] = env.get_env_attr(required_info[key])
